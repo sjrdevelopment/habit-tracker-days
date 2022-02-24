97da876f12b5a3c65968c6fa50530a5edf6b4d39
sjrdevelopment <stuj.robinson@gmail.com>
2022-02-24 16:01:07 +0000

    updating styles for days columns

--- src/components/habitItem/index.js
+++ src/components/habitItem/index.js
@@ -6,24 +6,26 @@ import TodayStatus from '../todayStatus'
 import HabitDelete from '../habitDelete'
 import './habitItem.css'
 
+const lettersOfWeek = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+
 const getLast2WeeksTableHeaders = () => {
-  const dateRange = Array(13)
+  const dateRange = Array(14)
     .fill()
     .map((item, index) => {
       const date = new Date()
-      date.setDate(date.getDate() - index - 1)
+      date.setDate(date.getDate() - index)
 
       const dayOfWeek = date.getDay()
 
       if (dayOfWeek === 0 || dayOfWeek === 6) {
         return (
-          <th className="weekend" key={index}>{`${date.getDate()}/${
-            date.getMonth() + 1
-          }`}</th>
+          <th className="weekend" key={index}>
+            {lettersOfWeek[dayOfWeek]}
+          </th>
         )
       }
 
-      return <th key={index}>{`${date.getDate()}/${date.getMonth() + 1}`}</th>
+      return <th key={index}>{lettersOfWeek[dayOfWeek]}</th>
     })
 
   return dateRange.reverse()
@@ -34,11 +36,7 @@ const HabitItem = ({ trackerData }) => {
     <div className="habit-item">
       <table>
         <thead>
-          <tr>
-            <th></th>
-            {getLast2WeeksTableHeaders()}
-            <th>Today</th>
-          </tr>
+          <tr>{getLast2WeeksTableHeaders()}</tr>
         </thead>
         <tbody>
           {trackerData.combinedItems &&
@@ -46,27 +44,32 @@ const HabitItem = ({ trackerData }) => {
               const finalDay = item.history.slice(-1)[0]
 
               return (
-                <tr key={index}>
-                  <td>
-                    {item.name} <HabitDelete item={item} />
-                  </td>
-                  {item.history.slice(0, 13).map((historyItem, index) => {
-                    if (historyItem.completed) {
+                <>
+                  <tr className="header-row">
+                    <td colspan="14">
+                      {item.name}
+                      <HabitDelete item={item} />
+                    </td>
+                  </tr>
+                  <tr className="data-row" key={index}>
+                    {item.history.slice(0, 13).map((historyItem, index) => {
+                      if (historyItem.completed) {
+                        return (
+                          <td key={index} className="l1">
+                            <span className="circle"></span>
+                          </td>
+                        )
+                      }
+
                       return (
-                        <td key={index} className="l1">
+                        <td key={index} className="l0">
                           <span className="circle"></span>
                         </td>
                       )
-                    }
-
-                    return (
-                      <td key={index} className="l0">
-                        <span className="circle"></span>
-                      </td>
-                    )
-                  })}
-                  <TodayStatus item={item} finalDay={finalDay} />
-                </tr>
+                    })}
+                    <TodayStatus item={item} finalDay={finalDay} />
+                  </tr>
+                </>
               )
             })}
         </tbody>
